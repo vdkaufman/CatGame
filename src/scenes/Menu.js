c69f33dc178273d20e4c180bd54dab5f4524f8ad
Keli Lindsey <kklindse@ucsc.edu>
2022-05-13 01:00:15 -0700

    Added Menu scene

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -3,12 +3,54 @@ class Menu extends Phaser.Scene {
         super("menuScene");
     }
     preload(){
+        this.load.spritesheet('TitleAnimation', './assets/TitleAnimation.png', 
+        {frameWidth: 1100, frameHeight: 393});
     }
     create() {
-        // pass to the next scene
-        this.scene.start('playScene');
+
+        // menu text configuration
+        let menuConfig = {
+            fontFamily: 'Courier',
+            fontSize: '24px',
+            backgroundColor: '#3b243e',
+            color: '#716d7b',
+            align: 'right',
+            padding: {
+                top: 5,
+                bottom: 5,
+            },
+            fixedWidth: 0
+        }
+
+        // Set Menu background color
+
+        this.add.rectangle(0, 0, game.config.width, game.config.height, 0x3b243e).setOrigin(0, 0);
+
+        // Show Title image
+
+        this.anims.create({
+            key: 'TitleAnimation',
+            frames: this.anims.generateFrameNumbers('TitleAnimation', {frames: [0,1]}),
+            frameRate: 4,
+            repeat:-1
+        });
+
+        this.startMenu = this.add.sprite(550, 300, 'TitleAnimation');
+        this.startMenu.play('TitleAnimation');
+
+        // Display Menu Text
+        this.add.text(game.config.width/2, 550, ' Press [SPACE] to start ', 
+        menuConfig).setOrigin(0.5);
+
+        // define keys
+        keySPACE = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+
     }
 
-    update(){
+    update() {
+        if (Phaser.Input.Keyboard.JustDown(keySPACE)) {
+        // pass to the next scene
+        this.scene.start('playScene');
+        }
     }
 }
